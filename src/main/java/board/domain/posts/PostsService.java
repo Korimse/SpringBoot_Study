36335485354887158e2494a64fa83f0e6d54fae7
siheon <korimse@wemade.com>
2021-05-04 16:00:14 +0900

    feat: mustache, 게시글 수정, 삭제, 화면

--- src/main/java/board/domain/posts/PostsService.java
+++ src/main/java/board/domain/posts/PostsService.java
@@ -1,11 +1,15 @@
 package board.domain.posts;
 
+import board.web.dto.PostsListResponseDto;
 import board.web.dto.PostsResponseDto;
 import board.web.dto.PostsSaveRequestDto;
 import board.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,19 @@ public class PostsService {
                 .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        postsRepository.delete(posts);
     }
 }
