91f8a74a3b601cf99fc1e8c0d5ac7d5b0fa3c0d4
siheon <korimse@wemade.com>
2021-05-04 10:48:26 +0900

    feat: JPA

---
+++ src/main/java/board/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package board.web.dto;
+
+import board.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts Entity){
+        this.id = Entity.getId();
+        this.title = Entity.getTitle();
+        this.content = Entity.getContent();
+        this.author = Entity.getAuthor();
+    }
+}
