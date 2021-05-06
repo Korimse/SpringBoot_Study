94abbf954ba9e8c6a99022419e566bede903f4d8
siheon <korimse@wemade.com>
2021-05-06 11:43:56 +0900

    feat: 네이버 로그인, 유닛 테스트

--- src/main/java/board/config/auth/dto/OAuthAttributes.java
+++ src/main/java/board/config/auth/dto/OAuthAttributes.java
@@ -28,15 +28,30 @@ public class OAuthAttributes {
 
     public static OAuthAttributes of(String registrationId, String userNameAttributesName,
                                      Map<String, Object> attributes){
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributesName, attributes);
     }
 
-    public static OAuthAttributes ofGoogle(String userNameAttributesName, Map<String , Object> attributes){
+    private static OAuthAttributes ofGoogle(String userNameAttributesName, Map<String , Object> attributes){
         return OAuthAttributes.builder().name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
                 .nameAttributesKey(userNameAttributesName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes){
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributesKey(userNameAttributeName)
                 .build();
     }
 
